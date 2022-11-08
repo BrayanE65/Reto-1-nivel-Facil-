38769891cfe23f85b9528a0fd26060865e6d3ea0
Brayan <bredcom@hotmail.com>
2022-11-08 18:51:54 -0500

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ *
+ */
+
+
+
+for (int i = 1; i < 101; i++)
+{
+    int r=3;
+    int p=5;
+    int k=i%p;
+    int j=i%r;
+    if (j==0 && k==0)
+    {
+        Console.WriteLine("fizz, buzz");
+    }
+    else if (k==0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else if(j==0)
+    {
+        Console.WriteLine("fizz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+    
+}
